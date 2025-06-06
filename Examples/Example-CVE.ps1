972ad172566fdc1f6d683fe62dccc71821cca975
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-06 09:16:19 +0200

    feat(Examples): ✨ add Example-CVE.ps1 and Example-ImproveEmailBody.ps1 for vulnerability and email optimization examples
    * Introduced `Example-CVE.ps1` to demonstrate vulnerability extraction from web pages.
    * Added `Example-ImproveEmailBody.ps1` to showcase email body optimization techniques.

---
+++ Examples/Example-CVE.ps1
@@ -0,0 +1,20 @@
+﻿$BaseUri = 'https://www.acunetix.com/vulnerabilities/web/page/'
+
+[Array] $Vulnerabilities = foreach ($page in (1..2)) {
+    $ProgressPreference = 'SilentlyContinue'
+    $html = (Invoke-WebRequest -Uri "$BaseUri/$page/").Content
+    #$AgilityPackTable = ConvertFrom-HtmlTable -Content $html -Engine AgilityPack -ReplaceHeaders $ReplaceHeaders
+    $AngleSharpTable = ConvertFrom-HtmlTable -Content $html -Engine AngleSharp -ReplaceHeaders $ReplaceHeaders
+
+    foreach ($row in $AngleSharpTable) {
+        $Vulnerability = [PSCustomObject]@{
+            'Vulnerability Name' = $row.'Vulnerability Name'
+            CVE                  = $row."CVE`n`nCWE"
+            CWE                  = $row.CWE
+            Severity             = $row.Severity
+        }
+        $Vulnerability
+    }
+}
+
+$Vulnerabilities | Format-Table
---
+++ Examples/Example-ImproveEmailBody.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module .\PSParseHTML.psd1 -Force
+
+$Body = EmailBody {
+    EmailText -Text 'This is a test email'
+}
+
+$Body = Optimize-Email -Body $Body -RemoveComments -RemoveStyleElements
+$Body = Format-HTML -Content $Body
+$Body
