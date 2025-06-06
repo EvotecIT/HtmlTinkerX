fee865203269a111a675e320276c414b703906ff
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-06 17:36:27 +0200

    feat: ✨ Add Version-Update script for module versioning
    * Introduced `Version-Update.ps1` to automate project version updates.
    * Utilizes `Get-ProjectVersion` and `Set-ProjectVersion` for managing versioning.

---
+++ Build/Version-Update.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module PSPublishModule
+
+Get-ProjectVersion -Path "C:\Support\GitHub\PSParseHTML" -ExcludeFolders "C:\Support\GitHub\PSParseHTML\Module\Artefacts"
+Set-ProjectVersion -Path "C:\Support\GitHub\PSParseHTML" -NewVersion "2.0.0"
