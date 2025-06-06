8ce7a099cce7b7a396024ef1c9dfefc9e2421983
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-06 09:15:14 +0200

    feat(CmdletOptimizeEmail): ✨ add CmdletOptimizeEmail for optimizing email content
    * Introduced a new cmdlet `Optimize-Email` to optimize HTML email content.
    * Supports parameters for removing comments and style elements.
    * Implements logging for warnings during processing.

---
+++ Sources/PSParseHTML.PowerShell/CmdletOptimizeEmail.cs
@@ -0,0 +1,45 @@
+﻿namespace PSParseHTML.PowerShell;
+
+[Cmdlet(VerbsCommon.Optimize, "Email", DefaultParameterSetName = "Body", SupportsShouldProcess = true)]
+[CmdletBinding()]
+public sealed class CmdletOptimizeEmail : PSCmdlet {
+    [Parameter(Mandatory = true, ParameterSetName = "Body")]
+    [Alias("Content")]
+    public string Body { get; set; }
+
+    [Parameter(Mandatory = false, ParameterSetName = "Body")]
+    public SwitchParameter RemoveComments { get; set; }
+
+    [Parameter(Mandatory = false, ParameterSetName = "Body")]
+    public SwitchParameter RemoveStyleElements { get; set; }
+
+    private ActionPreference errorAction;
+
+    protected override void BeginProcessing() {
+        // Initialize the logger to be able to see verbose, warning, debug, error, progress, and information messages.
+        var internalLogger = new InternalLogger();
+        var internalLoggerPowerShell = new InternalLoggerPowerShell(internalLogger, this.WriteVerbose, this.WriteWarning, this.WriteDebug, this.WriteError, this.WriteProgress, this.WriteInformation);
+        LoggingMessages.Logger = internalLogger;
+
+        // Get the error action preference as user requested
+        // It first sets the error action to the default error action preference
+        // If the user has specified the error action, it will set the error action to the user specified error action
+        errorAction = (ActionPreference)this.SessionState.PSVariable.GetValue("ErrorActionPreference");
+        if (this.MyInvocation.BoundParameters.ContainsKey("ErrorAction")) {
+            string errorActionString = this.MyInvocation.BoundParameters["ErrorAction"].ToString();
+            if (Enum.TryParse(errorActionString, true, out ActionPreference actionPreference)) {
+                errorAction = actionPreference;
+            }
+        }
+    }
+
+    protected override void ProcessRecord() {
+        var result = PreMailer.Net.PreMailer.MoveCssInline(Body, removeComments: RemoveComments, removeStyleElements: RemoveStyleElements);
+        WriteObject(result.Html);
+
+        // Log the information message
+        foreach (var warning in result.Warnings) {
+            LoggingMessages.Logger.WriteWarning(warning);
+        }
+    }
+}
