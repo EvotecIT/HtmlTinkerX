da296b8c6d55cbef6638b8b4c269eed97c552632
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-07 20:58:16 +0200

    #12

---
+++ Examples/Example-ConvertFromTableHeadless.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module '.\PSParseHTML.psd1' -Force
+
+$Path = Join-Path $PSScriptRoot '\Input\headless_table.html'
+$Content = Get-Content -LiteralPath $Path -Raw
+
+$Tables = ConvertFrom-HtmlTable -Content $Content
+$Tables[0] | Format-Table -AutoSize
+
+$Tables[1] | Format-Table -AutoSize
+
+$Tables[2] | Format-Table -AutoSize
