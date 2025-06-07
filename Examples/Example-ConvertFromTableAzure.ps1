4952dccc6c82dbba2263408d9fb5c73b3846012f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-07 20:18:11 +0200

    feat: ✨ Add example script for converting Azure status table to PowerShell objects
    * Introduced `Example-ConvertFromTableAzure.ps1` to demonstrate the usage of `ConvertFrom-HtmlTable` for parsing Azure status data.
    * The script fetches and formats Azure status information, showcasing the capabilities of the HTML table parsing functionality.

---
+++ Examples/Example-ConvertFromTableAzure.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module '.\PSParseHTML.psd1' -Force
+
+$AzureStatus = ConvertFrom-HtmlTable -Url "https://status.azure.com/en-us/status" -IncludeMetadata -AllProperties -SkipFooter
+$AzureStatus[1].Data | Format-Table -AutoSize
+
+$AzureStatus1 = ConvertFrom-HtmlTable -Url "https://status.azure.com/en-us/status" -AllProperties -SkipFooter
+$AzureStatus1[1] | Format-Table -AutoSize
