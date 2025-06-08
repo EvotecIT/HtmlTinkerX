09986d7f5ad7ce53ef7017dae38b42b82161f921
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-08 18:12:35 +0200

    Refactor Example-DownloadItems.ps1 to streamline download commands by consolidating file download options and removing commented-out code.

--- Examples/Example-DownloadItems.ps1
+++ Examples/Example-DownloadItems.ps1
@@ -1,15 +1,10 @@
 ﻿Import-Module .\PSParseHTML.psd1 -Force
 
-# $Lists = ConvertFrom-HtmlList -Content $Items -AsObject
-# $Lists.Count
-# $Lists[0] | Format-Table
-# $Lists[1] | Format-Table
-
+# Download all files
 Save-HTMLDownload -Url 'https://github.com/EvotecIT/DnsClientX/releases/tag/DnsClientX-PowerShellModule.v0.4.0' -Path "$PSScriptRoot\Output\Test" -Verbose
+# Download specific file
 Save-HTMLDownload -Url 'https://github.com/EvotecIT/DnsClientX/releases/tag/DnsClientX-PowerShellModule.v0.4.0' -Path "$PSScriptRoot\Output\Test" -Filter 'DnsClientX-PowerShellModule.v0.4.0.zip'
+# Download all zip files
 Save-HTMLDownload -Url 'https://github.com/EvotecIT/DnsClientX/releases/tag/DnsClientX-PowerShellModule.v0.4.0' -Path "$PSScriptRoot\Output\Test" -Filter ".zip"
+# Download all tar.gz files
 Save-HTMLDownload -Url 'https://github.com/EvotecIT/DnsClientX/releases/tag/DnsClientX-PowerShellModule.v0.4.0' -Path "$PSScriptRoot\Output\Test" -Filter ".tar.gz"
-
-
-
-
