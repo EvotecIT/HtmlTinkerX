2babeb57d37f76e130eee0696b6147310aaeef5b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-08 09:41:53 +0200

    feat: ✨ Add Example-GetDynamicHTML.ps1 for dynamic HTML rendering demonstration
    * Introduced a new PowerShell script to demonstrate dynamic HTML rendering using the Get-RenderedHtml function.
    * The script retrieves HTML content from a specified URL and saves it to a file, showcasing the functionality of the PSParseHTML module.

---
+++ Examples/Example-GetDynamicHTML.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module .\PSParseHTML.psd1 -Force
+
+$HTML = Get-RenderedHtml -Url "https://www.evotec.xyz"
+$HTML
+
+$HTML = Get-RenderedHtml -Url "https://www.evotec.xyz" -OutFile "$PSScriptRoot\Output\evotec.html"
+$HTML
