3410ebeadf5ecd730a41560d9154c546944ef440
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-08 22:30:28 +0200

    refactor(examples): ✏️ clean up Example-GetDynamicHTML.ps1
    * Removed unnecessary warnings and comments for clarity.
    * Streamlined the example usage of `Invoke-HTMLRendering`.

--- Examples/Example-GetDynamicHTML.ps1
+++ Examples/Example-GetDynamicHTML.ps1
@@ -1,23 +1,7 @@
 ﻿Import-Module .\PSParseHTML.psd1 -Force
 
-<#
-WARNING: Development mode: Using binaries from C:\Support\GitHub\PSParseHTML\Sources\PSParseHTML.PowerShell\bin\Debug\net8.0\PSParseHTML.PowerShell.dll
-Downloading Chromium 136.0.7103.25 (playwright build v1169) from https://cdn.playwright.dev/dbazure/download/playwright/builds/chromium/1169/chromium-win64.zip
-144.4 MiB [====================] 100% 0.0s
-Chromium 136.0.7103.25 (playwright build v1169) downloaded to C:\Users\przemyslaw.klys\AppData\Local\ms-playwright\chromium-1169
-Downloading FFMPEG playwright build v1011 from https://cdn.playwright.dev/dbazure/download/playwright/builds/ffmpeg/1011/ffmpeg-win64.zip
-1.3 MiB [====================] 100% 0.0s
-FFMPEG playwright build v1011 downloaded to C:\Users\przemyslaw.klys\AppData\Local\ms-playwright\ffmpeg-1011
-Downloading Chromium Headless Shell 136.0.7103.25 (playwright build v1169) from https://cdn.playwright.dev/dbazure/download/playwright/builds/chromium/1169/chromium-headless-shell-win64.zip
-89.1 MiB [====================] 100% 0.0s
-Chromium Headless Shell 136.0.7103.25 (playwright build v1169) downloaded to C:\Users\przemyslaw.klys\AppData\Local\ms-playwright\chromium_headless_shell-1169
-Downloading Winldd playwright build v1007 from https://cdn.playwright.dev/dbazure/download/playwright/builds/winldd/1007/winldd-win64.zip
-0.1 MiB [====================] 100% 0.0s
-Winldd playwright build v1007 downloaded to C:\Users\przemyslaw.klys\AppData\Local\ms-playwright\winldd-1007
-#>
-
+# Example of using Invoke-HTMLRendering to get dynamic HTML content
 $HTML = Invoke-HTMLRendering -Url "https://www.evotec.xyz"
 $HTML
-
-#$HTML = Invoke-HTMLRendering -Url "https://www.evotec.xyz" -OutFile "$PSScriptRoot\Output\evotec.html"
-#$HTML
+# Save HTML to file
+Invoke-HTMLRendering -Url "https://www.evotec.xyz" -OutFile "$PSScriptRoot\Output\evotec.html"
