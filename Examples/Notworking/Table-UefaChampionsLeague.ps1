f8646f4789a029763d4011dae2af228642674ead
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-08 11:14:05 +0200

    refactor: 🔄 Remove outdated Example-Windows10Versions.ps1 and enhance Table-UefaChampionsLeague.ps1 for multiple URLs
    * Deleted Example-Windows10Versions.ps1 as it was no longer relevant.
    * Updated Table-UefaChampionsLeague.ps1 to handle multiple URLs for retrieving and formatting UEFA Champions League data.

--- Examples/Notworking/Table-UefaChampionsLeague.ps1
+++ Examples/Notworking/Table-UefaChampionsLeague.ps1
@@ -1,6 +1,16 @@
 ﻿Import-Module .\PSParseHTML.psd1 -Force
 
-$Test = ConvertFrom-HtmlTable -Url 'https://www.goal.com/en-us/uefa-champions-league/table/4oogyu6o156iphvdvphwpck10'
-for ($Count = 0; $Count -lt $Test.Count; $Count++) {
-    $Test[$Count] | Format-Table -AutoSize *
+$Urls = @(
+    'https://www.goal.com/en-us/champions-league/4oogyu6o156iphvdvphwpck10'
+    'https://www.goal.com/en-us/champions-league/fixtures-results/4oogyu6o156iphvdvphwpck10'
+    'https://www.goal.com/en-us/champions-league/table/4oogyu6o156iphvdvphwpck10'
+    'https://www.goal.com/en-us/champions-league/top-players/4oogyu6o156iphvdvphwpck10'
+)
+foreach ($Url in $Urls) {
+    $HTML = Invoke-HTMLRendering -Url $Url
+
+    $Test = ConvertFrom-HtmlTable -Content $HTML
+    for ($Count = 0; $Count -lt $Test.Count; $Count++) {
+        $Test[$Count] | Format-Table -AutoSize *
+    }
 }
