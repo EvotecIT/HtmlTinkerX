95b88a687d3e8d5e13d29ba47cb62a1262e33a3e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-08 13:18:53 +0200

    Added Example-Screenshot1.ps1 to demonstrate HTML screenshot capturing using Save-HTMLScreenshot cmdlet for specified URLs.

---
+++ Examples/Example-Screenshot1.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module .\PSParseHTML.psd1 -Force
+
+Save-HTMLScreenshot -Url 'https://www.goal.com/en-us/premier-league/table/2kwbbcootiqqgmrzs6o5inle5' -OutFile "$PSScriptRoot\Output\PremierLeague.png"
+
+Save-HTMLScreenshot -Url 'https://evotec.xyz' -OutFile "$PSScriptRoot\Output\EvotecPage.png"
