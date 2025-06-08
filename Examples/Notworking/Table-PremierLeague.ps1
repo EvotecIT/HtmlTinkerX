c303b7be38fa8f870a50bf9967fe342ae921a20b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-08 11:03:57 +0200

    feat: ✨ Add examples for converting HTML tables from COVID-19 and Premier League data
    * Introduced Example-Table-CovidCasesAustralia.ps1 to extract and format COVID-19 data from a specified URL.
    * Added Example-Table-PremierLeague.ps1 to retrieve and display Premier League table data using different engines.
    * Removed outdated examples from the Notworking directory to streamline the codebase.

--- Examples/Notworking/Table-PremierLeague.ps1
+++
@@ -1,8 +0,0 @@
-﻿Import-Module .\PSParseHTML.psd1 -Force
-
-$Test = ConvertFrom-HtmlTable -Url 'https://www.goal.com/en-us/premier-league/table/2kwbbcootiqqgmrzs6o5inle5'
-$Test | Format-Table -AutoSize *
-
-
-$Test = ConvertFrom-HtmlTable -Url 'https://www.goal.com/en-us/premier-league/table/2kwbbcootiqqgmrzs6o5inle5' -Engine AngleSharp
-$Test | Format-Table -AutoSize *
