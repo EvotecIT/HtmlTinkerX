3076873d483bf56f3ca70930c0c832cb1077e3ea
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-08 17:14:57 +0200

    Remove verbose output from Save-HTMLDownload in Example-DownloadItems.ps1

--- Examples/Example-DownloadItems.ps1
+++ Examples/Example-DownloadItems.ps1
@@ -5,7 +5,7 @@
 # $Lists[0] | Format-Table
 # $Lists[1] | Format-Table
 
-Save-HTMLDownload -Url 'https://github.com/EvotecIT/DnsClientX/releases/tag/DnsClientX-PowerShellModule.v0.4.0' -Path "$PSScriptRoot\Output\Test" -Filter 'DnsClientX-PowerShellModule.v0.4.0.zip' -Verbose
+Save-HTMLDownload -Url 'https://github.com/EvotecIT/DnsClientX/releases/tag/DnsClientX-PowerShellModule.v0.4.0' -Path "$PSScriptRoot\Output\Test" -Filter 'DnsClientX-PowerShellModule.v0.4.0.zip'
 Save-HTMLDownload -Url 'https://github.com/EvotecIT/DnsClientX/releases/tag/DnsClientX-PowerShellModule.v0.4.0' -Path "$PSScriptRoot\Output\Test" -Filter ".zip"
 Save-HTMLDownload -Url 'https://github.com/EvotecIT/DnsClientX/releases/tag/DnsClientX-PowerShellModule.v0.4.0' -Path "$PSScriptRoot\Output\Test" -Filter ".tar.gz"
 
