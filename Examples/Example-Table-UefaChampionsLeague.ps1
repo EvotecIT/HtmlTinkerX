ac57728a9bb7d126309dafc18b56d8cd7b243295
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-08 11:51:43 +0200

    feat: ✨ Add Example-Table-UefaChampionsLeague.ps1 for HTML table extraction
    * Introduced a new PowerShell script to retrieve and format UEFA Champions League data from multiple URLs using the PSParseHTML module.
    * The script utilizes Invoke-HTMLRendering and ConvertFrom-HtmlTable to display the data in a structured table format.

--- Examples/Example-Table-UefaChampionsLeague.ps1
+++ Examples/Example-Table-UefaChampionsLeague.ps1
@@ -7,7 +7,7 @@ $Urls = @(
     'https://www.goal.com/en-us/champions-league/top-players/4oogyu6o156iphvdvphwpck10'
 )
 foreach ($Url in $Urls) {
-    $HTML = Invoke-HTMLRendering -Url $Url
+    $HTML = Invoke-HTMLRendering -Url $Url -Browser Chromium
 
     $Test = ConvertFrom-HtmlTable -Content $HTML
     for ($Count = 0; $Count -lt $Test.Count; $Count++) {
