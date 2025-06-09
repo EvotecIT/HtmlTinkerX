bba8d66a7fc419778d60552408e3efac00d6ce0c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-09 16:59:14 +0200

    feat(examples): add Example-BrowserSessionWpAdmin.ps1 for WordPress session management
    * Introduced a new PowerShell script to demonstrate browser session management for WordPress admin.
    * Implemented cmdlets for logging in, navigating, and taking screenshots of various admin pages.
    * Enhanced session handling with the ability to save screenshots during navigation.

---
+++ Examples/Example-BrowserSessionWpAdmin.ps1
@@ -0,0 +1,27 @@
+﻿Import-Module .\PSParseHTML.psd1 -Force
+
+# Credentials
+$Credentials = [PSCredential]::new('TestUser', (ConvertTo-SecureString -String $Env:WordpressPassword -AsPlainText -Force))
+# Use Invoke-HTMLRendering to create a session, alternatively use Start-HTMLSession, Open-HTMLSession which should be an alias for Invoke-HTMLRendering
+$invokeHTMLRenderingSplat = @{
+    Url              = 'https://evotec.xyz/wp-admin'
+    LoginUrl         = 'https://evotec.xyz/wp-login.php'
+    UsernameSelector = '#user_login'
+    PasswordSelector = '#user_pass'
+    SubmitSelector   = '#wp-submit'
+    Credential       = $Credentials
+    Session          = $true
+}
+$session = Invoke-HTMLRendering @invokeHTMLRenderingSplat
+# Save screenshot of the page should work with session
+Save-HTMLScreenshot -Session $Session -OutFile "$PSScriptRoot\Output\EvotecPageAdmin1.png" -Open
+# We should add new cmdlet that will navigate to the page we tell it to navigate to
+Invoke-HTMLNavigation -Session $Session -Url 'https://evotec.xyz/wp-admin/edit.php'
+# Save screenshot of the page should work with session
+Save-HTMLScreenshot -Session $Session -OutFile "$PSScriptRoot\Output\EvotecPageAdmin2.png" -Open
+# We should add new cmdlet that will navigate to the page we tell it to navigate to, but also allow Save-HTMLScreenshot to work with session from the page we navigate to
+Invoke-HTMLNavigation -Session $Session -Url 'https://evotec.xyz/wp-admin/edit.php' | Save-HTMLScreenshot -OutFile "$PSScriptRoot\Output\EvotecPageAdmin3.png" -Open
+# Navigate to plugins page and save screenshot
+Invoke-HTMLNavigation -Session $Session -Url 'https://evotec.xyz/wp-admin/edit.php?post_type=page' | Save-HTMLScreenshot -OutFile "$PSScriptRoot\Output\EvotecPageAdmin4.png" -Open
+# Close the session using new cmdlet alias (Stop-HTMLSession)
+Close-HTMLSession -Session $Session | Out-Null
