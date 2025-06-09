889aa398eec83300541bc062c9679cace22c32f7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-09 18:15:52 +0200

    feat(examples): add Example-BrowserSessionInteractable.ps1 for HTML session interaction
    * Introduced a new PowerShell script that demonstrates interacting with HTML sessions using the PSParseHTML module.
    * The script showcases starting a session, capturing a screenshot, and retrieving interactable elements from a webpage and a local HTML file.

---
+++ Examples/Example-BrowserSessionInteractable.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module .\PSParseHTML.psd1 -Force
+
+$Session = Start-HTMLSession -Url 'https://evotec.xyz' -Session
+Save-HTMLScreenshot -Session $Session -OutFile "$PSScriptRoot\Output\Interactable1.png" -Open
+Get-HTMLInteractable -Session $Session | Format-Table
+
+Get-HTMLInteractable -Url 'https://evotec.xyz' | Format-Table
+
+Get-HTMLInteractable -Path "$PSScriptRoot\Input\azure_status.html" | Format-Table
