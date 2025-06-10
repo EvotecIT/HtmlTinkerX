671548336ffc163e4ff71c38a93ac50db0115101
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-10 19:22:28 +0200

    feat(Examples): ✨ Enhance browser session examples with navigation and screenshot functionality
    * Added `Invoke-HTMLNavigation` to navigate to specific pages.
    * Included `Save-HTMLScreenshot` after navigation to capture page state.
    * Removed outdated example file to streamline the examples directory.

--- Examples/Example-BrowserSessionInteractable.ps1
+++ Examples/Example-BrowserSessionInteractable.ps1
@@ -7,5 +7,8 @@ Save-HTMLScreenshot -Session $Session -OutFile "$PSScriptRoot\Output\Interactabl
 Get-HTMLInteractable -Session $Session | Format-Table
 # Alternatively, you can use the URL directly without a session (if you don't need to maintain a session state)
 Get-HTMLInteractable -Url 'https://evotec.xyz' | Format-Table
+Invoke-HTMLNavigation -Session $Session -Selector "a[href='https://evotec.xyz/docs/']"
+# Save a screenshot of the page after navigating to the documentation page
+Save-HTMLScreenshot -Session $Session -OutFile "$PSScriptRoot\Output\Interactable2.png" -Open
 # You can also get interactable elements from a local HTML file
 Get-HTMLInteractable -Path "$PSScriptRoot\Input\azure_status.html" | Format-Table
