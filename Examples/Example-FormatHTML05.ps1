e4dd119d6585992acc2bf833486aa8f0a4478dd8
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-12 20:47:37 +0200

    fix(Examples): Update output file paths in Example-FormatHTML05.ps1
    * Changed output file paths for generated HTML files to the Output directory for better organization.
    * Ensured consistency in file handling by directing outputs to the appropriate location.

--- Examples/Example-FormatHTML05.ps1
+++ Examples/Example-FormatHTML05.ps1
@@ -4,5 +4,5 @@ New-HTML -Online {
     New-HTMLTable -DataTable 'test'
 } -FilePath $PSScriptRoot\Input\Test.html #-ShowHTML
 
-Format-HTML -File $PSScriptRoot\Input\Test.html -OutputFile $PSScriptRoot\Input\Test1.html
-Optimize-HTML -File $PSScriptRoot\Input\Test.html -OutputFile $PSScriptRoot\Input\Test2.html
+Format-HTML -File $PSScriptRoot\Input\Test.html -OutputFile $PSScriptRoot\Output\Test1.html
+Optimize-HTML -File $PSScriptRoot\Input\Test.html -OutputFile $PSScriptRoot\Output\Test2.html
