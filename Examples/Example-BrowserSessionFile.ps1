c0c4a08e67283caf90b00e88c818cedb80625a27
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-12 20:23:27 +0200

    feat(Examples): ✨ Add new example scripts for HTML session and screenshot functionality
    * Introduced `Example-BrowserSessionFile.ps1` to demonstrate HTML session handling and interaction.
    * Added `Example-ScreenshotLocal.ps1` to showcase diagram creation and screenshot capture from HTML content.
    * Both examples utilize the PSWriteHTML module for enhanced visual representation.

---
+++ Examples/Example-BrowserSessionFile.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module .\PSParseHTML.psd1 -Force
+
+# When using Session, you can either save $Session variable or use the "default" session
+# Default session is always used unless you specify NoSession
+$null = Open-HTMLSession -Path "$PSScriptRoot\Input\Example-HierarchicalLayout01.html" -Session
+
+Save-HTMLScreenshot -OutFile "$PSScriptRoot\Output\HierarchicalLayout01.png" -Full -Open
+
+Get-HTMLInteractable -IncludeHidden | Format-Table
+
+Invoke-HTMLNavigation -Selector "a#hide_anchor-y4b615h"
+
+Save-HTMLScreenshot -OutFile "$PSScriptRoot\Output\HierarchicalLayout02.png" -Full -Open
