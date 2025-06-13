f0ea5cf5a6f8c2caa14f2daa7809deb0d4bfae91
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-13 18:07:17 +0200

    Add table example skipping footer

--- Examples/Example-ConvertFromTableWithFooter.ps1
+++ Examples/Example-ConvertFromTableWithFooter.ps1
@@ -1,1 +1,7 @@
-﻿
+﻿Import-Module './PSParseHTML.psd1' -Force
+
+$Path = Join-Path $PSScriptRoot 'Input\easy_table_with_footer.html'
+$Content = Get-Content -LiteralPath $Path -Raw
+
+$Tables = ConvertFrom-HtmlTable -Content $Content -SkipFooter
+$Tables[0] | Format-Table -AutoSize
