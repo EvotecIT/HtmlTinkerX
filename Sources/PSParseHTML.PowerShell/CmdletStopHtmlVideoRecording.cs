aa1e04d568cb319d731ae0f42b5164190f67d928
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-13 22:12:41 +0200

    fix: default session support for Stop-HTMLVideoRecording

--- Sources/PSParseHTML.PowerShell/CmdletStopHtmlVideoRecording.cs
+++ Sources/PSParseHTML.PowerShell/CmdletStopHtmlVideoRecording.cs
@@ -3,18 +3,22 @@ using System.Threading.Tasks;
 
 namespace PSParseHTML.PowerShell;
 
+
 [Cmdlet(VerbsLifecycle.Stop, "HTMLVideoRecording")]
 public sealed class CmdletStopHtmlVideoRecording : AsyncPSCmdlet {
-    [Parameter(Mandatory = true, ValueFromPipeline = true, Position = 0)]
-    public HtmlBrowserSession Session { get; set; } = null!;
+    [Parameter(ValueFromPipeline = true, Position = 0)]
+    public HtmlBrowserSession? Session { get; set; }
 
     [Parameter]
     public string? OutFile { get; set; }
 
     protected override async Task ProcessRecordAsync() {
-        await HtmlBrowser.StopVideoRecordingAsync(Session, OutFile).ConfigureAwait(false);
+        HtmlBrowserSession session = Session ?? (HtmlBrowserSession?)GetVariableValue("PSParseHTML_DefaultSession")
+            ?? throw new PSInvalidOperationException("No session provided and no default session found.");
+
+        await HtmlBrowser.StopVideoRecordingAsync(session, OutFile).ConfigureAwait(false);
         object? defaultSession = GetVariableValue("PSParseHTML_DefaultSession");
-        if (defaultSession is HtmlBrowserSession sess && ReferenceEquals(sess, Session)) {
+        if (defaultSession is HtmlBrowserSession sess && ReferenceEquals(sess, session)) {
             SessionState.PSVariable.Remove("PSParseHTML_DefaultSession");
         }
     }
