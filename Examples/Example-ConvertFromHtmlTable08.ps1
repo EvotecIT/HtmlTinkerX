6bd499f48213af0ecc4b56094302be691779129d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-13 21:23:29 +0200

    feat: ✨ Add example script for converting HTML tables
    * Introduced `Example-ConvertFromHtmlTable08.ps1` to demonstrate the usage of `ConvertFrom-HtmlTable`.
    * The script imports the necessary module and processes a sample HTML file.

---
+++ Examples/Example-ConvertFromHtmlTable08.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module .\PSParseHTML.psd1 -Force
+
+$Path = "$PSScriptRoot\Input\Test.html"
+
+ConvertFrom-HtmlTable -Content (Get-Content -LiteralPath $Path -Raw) -SkipFooter | Format-Table
