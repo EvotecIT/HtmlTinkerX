71fca10267c1b005b61f2a384432c9c8f333c41d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-14 18:41:20 +0200

    feat(video): 🎥 Enhance video recording and navigation commands
    * Added new cmdlets for navigating to specific pages during video recording.
    * Implemented `Stop-HTMLVideoRecording` to properly close video sessions.
    * Improved session management with `Close-HTMLSession` alias.

--- Examples/Example-Video.ps1
+++ Examples/Example-Video.ps1
@@ -13,11 +13,22 @@ $sessionParams = @{
 }
 
 $session = Open-HTMLSession @sessionParams
-
-Save-HTMLScreenshot -Session $session -OutFile "$PSScriptRoot\Output\WP1.png" -Open
-
 $session = Start-HTMLVideoRecording -Session $session -OutFile "$PSScriptRoot\Output\WP1.webm"
-
+# Get interactable elements from the session
 Get-HTMLInteractable -Session $session -Filter "Media" -IncludeHidden | Format-Table
-
+# # We should add new cmdlet that will navigate to the page we tell it to navigate to
+Invoke-HTMLNavigation -Session $Session -Url 'https://evotec.xyz/wp-admin/edit.php'
+# # We should add new cmdlet that will navigate to the page we tell it to navigate to
+Invoke-HTMLNavigation -Session $Session -Url 'https://evotec.xyz/wp-admin/edit.php'
+# # Navigate to plugins page and save screenshot
+Invoke-HTMLNavigation -Session $Session -Url 'https://evotec.xyz/wp-admin/edit.php?post_type=page'
+# # Navigate to team members page and save screenshot
+Invoke-HTMLNavigation -Session $Session -Url 'https://evotec.xyz/wp-admin/edit.php?post_type=thegem_team_person'
+# Navigate to profile page, this should error because of multiple elements with the same text
+Invoke-HTMLNavigation -Session $Session -Text "Profile"
+# Be exact with the text to avoid multiple elements with the same text
+Invoke-HTMLNavigation -Session $Session -Text "Profile" -Exact
+# Close video recording
 Stop-HTMLVideoRecording -Session $session
+# Close the session using new cmdlet alias (Stop-HTMLSession)
+Close-HTMLSession -Session $Session | Out-Null
