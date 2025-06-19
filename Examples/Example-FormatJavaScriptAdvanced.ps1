13663328024cec0f9d53c842f7e43d0328e6f8e4
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-19 21:59:12 +0200

    docs(examples): 📝 expand JavaScript options example

--- Examples/Example-FormatJavaScriptAdvanced.ps1
+++ Examples/Example-FormatJavaScriptAdvanced.ps1
@@ -1,5 +1,27 @@
 Import-Module .\PSParseHTML.psd1 -Force
 
-$Script = 'function greet(name){var data=[name];return data.map(function(x){return "Hello "+x;}).join().toUpperCase();}greet("World");'
+$Script = @'
+function greet(name){
+    var data = [name];
+    return data.map(function(x){
+        return "Hello " + x;
+    }).join().toUpperCase();
+}
 
-Format-JavaScript -Content $Script -IndentSize 2 -BraceStyle Expand -IndentWithTabs -KeepArrayIndentation -BreakChainedMethods
+greet("World");
+'@
+
+$opts = @{
+    Content              = $Script
+    IndentSize           = 2
+    IndentChar           = ' '
+    BraceStyle           = 'Expand'
+    IndentWithTabs       = $false
+    KeepArrayIndentation = $true
+    KeepFunctionIndentation = $false
+    BreakChainedMethods  = $true
+    MaxPreserveNewlines  = 2
+    JslintHappy          = $true
+}
+
+Format-JavaScript @opts
