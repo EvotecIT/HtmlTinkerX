d2488251335c410627d4253b5fdd51755455ecb1
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-19 21:59:06 +0200

    fix(examples): 🐛 remove backslashes from JavaScript formatting example

--- Examples/Example-FormatJavaScriptAdvanced.ps1
+++ Examples/Example-FormatJavaScriptAdvanced.ps1
@@ -2,9 +2,4 @@ Import-Module .\PSParseHTML.psd1 -Force
 
 $Script = 'function greet(name){var data=[name];return data.map(function(x){return "Hello "+x;}).join().toUpperCase();}greet("World");'
 
-Format-JavaScript -Content $Script \
-    -IndentSize 2 \
-    -BraceStyle Expand \
-    -IndentWithTabs \
-    -KeepArrayIndentation \
-    -BreakChainedMethods
+Format-JavaScript -Content $Script -IndentSize 2 -BraceStyle Expand -IndentWithTabs -KeepArrayIndentation -BreakChainedMethods
