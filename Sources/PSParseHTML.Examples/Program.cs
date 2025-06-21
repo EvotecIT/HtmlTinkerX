0d188ec58ec7ade92db3d5cf1be871648a3bbcf0
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-21 08:30:40 +0200

    feat(examples): ✨ add dedicated ShowHtmlHarExample class

--- Sources/PSParseHTML.Examples/Program.cs
+++ Sources/PSParseHTML.Examples/Program.cs
@@ -1,18 +1,9 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 
 namespace PSParseHTML.Examples;
 
 public static class Program
 {
-    public static async Task Main()
-    {
-        string har = Path.Combine("..", "..", "..", "Examples", "example.har");
-        Har data = await HtmlHarViewer.ReadHarAsync(har);
-        string html = HtmlHarViewer.BuildViewerHtml(data);
-        string outfile = Path.Combine("viewer.html");
-        await File.WriteAllTextAsync(outfile, html);
-        Console.WriteLine($"Viewer written to {outfile}");
-    }
+    public static Task Main()
+        => ShowHtmlHarExample.RunAsync();
 }
