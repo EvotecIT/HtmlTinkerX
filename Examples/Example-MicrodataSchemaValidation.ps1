419458767be6ba3a520fd93a2ab82e756cd8f75a
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-18 07:07:26 +0200

    Fix leftover namespace references

--- Examples/Example-MicrodataSchemaValidation.ps1
+++ Examples/Example-MicrodataSchemaValidation.ps1
@@ -1,5 +1,5 @@
 # Demonstrates validating microdata items with schema definitions
 $markup = '<div itemscope itemtype="https://schema.org/Person"><span itemprop="foo">bar</span></div>'
 $items = ConvertFrom-HtmlMicrodata -Content $markup
-$mismatches = [PSParseHTML.HtmlParser]::ValidateMicrodataItems($items)
+$mismatches = [HtmlTinkerX.HtmlParser]::ValidateMicrodataItems($items)
 $mismatches | Format-Table
