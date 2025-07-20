65b20d4c800a9fd4936dce13f1256753e689c1c2
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-07-20 15:12:17 +0200

    Refactor JS beautifier tests to use xUnit
    - Updated test class to use xUnit framework instead of NUnit.
    - Removed license header from the test file.
    - Simplified test setup by initializing the beautifier directly in each test method.
    - Updated assertions to use xUnit's Assert.Equal method.

--- Sources/HtmlTinkerX.Tests/TestJSBeautifierIndentation.cs
+++ Sources/HtmlTinkerX.Tests/TestJSBeautifierIndentation.cs
@@ -1,52 +1,30 @@
-﻿#region License
-// MIT License
-//
-// Copyright (c) 2018 Denis Ivanov
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-#endregion
+﻿using HtmlTinkerX.JavaScriptBeautifier;
+using Xunit;
 
-namespace Tests {
-    using Jsbeautifier;
-    using NUnit.Framework;
+namespace PSParseHTML.Tests;
 
-    [TestFixture]
-    public class TestJSBeautifierIndentation {
-        private Beautifier beautifier;
+/// <summary>
+/// Tests for JavaScript beautifier indentation options.
+/// </summary>
+public class JavaScriptBeautifierIndentationTests {
+    [Fact]
+    public void TestTabs() {
+        var beautifier = new Beautifier();
+        beautifier.Opts.IndentWithTabs = true;
 
-        public TestJSBeautifierIndentation() {
-            beautifier = new Beautifier();
-            BeautifierOptions bo = beautifier.Opts;
-            bo.IndentWithTabs = true;
-        }
+        string result = beautifier.Beautify("{tabs()}");
 
-        [Test]
-        public void TestTabs() {
-            Assert.AreEqual(beautifier.Beautify("{tabs()}"), "{\n\ttabs()\n}");
-        }
+        Assert.Equal("{\n\ttabs()\n}", result);
+    }
 
-        [Test]
-        public void TestFunctionIndent() {
-            BeautifierOptions bo = beautifier.Opts;
-            bo.IndentWithTabs = true;
-            bo.KeepArrayIndentation = true;
-            Assert.AreEqual(beautifier.Beautify("var foo = function(){ bar() }();"), "var foo = function() {\n\tbar()\n}();");
-        }
+    [Fact]
+    public void TestFunctionIndent() {
+        var beautifier = new Beautifier();
+        beautifier.Opts.IndentWithTabs = true;
+        beautifier.Opts.KeepArrayIndentation = true;
+
+        string result = beautifier.Beautify("var foo = function(){ bar() }();");
+
+        Assert.Equal("var foo = function() {\n\tbar()\n}();", result);
     }
 }
