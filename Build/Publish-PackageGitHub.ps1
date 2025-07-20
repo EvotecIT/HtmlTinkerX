f673a9bb6fa41df1a5954484d3271796b9af680b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-07-20 23:00:28 +0200

    feat(build): ✨ Add build scripts for GitHub and NuGet package publishing
    * Introduced `Build-Package.ps1` for .NET release builds.
    * Added `Publish-PackageGitHub.ps1` for publishing to GitHub.
    * Created `Publish-PackageNuget.ps1` for NuGet package publishing.

---
+++ Build/Publish-PackageGitHub.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module PSPublishModule -Force -ErrorAction Stop
+
+$GitHubAccessToken = Get-Content -Raw 'C:\Support\Important\GithubAPI.txt'
+
+$publishGitHubReleaseAssetSplat = @{
+    ProjectPath          = "$PSScriptRoot\..\Sources\HtmlTinkerX"
+    GitHubAccessToken    = $GitHubAccessToken
+    GitHubUsername       = "EvotecIT"
+    GitHubRepositoryName = "HtmlTinkerX"
+    IsPreRelease         = $false
+}
+
+Publish-GitHubReleaseAsset @publishGitHubReleaseAssetSplat
