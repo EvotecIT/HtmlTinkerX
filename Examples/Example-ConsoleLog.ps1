897ee01eccad27b35c4458df76cb13e024989c0b
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-21 18:55:03 +0200

    feat(console): ✨ add severity filter for console logs

--- Examples/Example-ConsoleLog.ps1
+++ Examples/Example-ConsoleLog.ps1
@@ -4,6 +4,7 @@ $path = Join-Path $PSScriptRoot 'Input/console_page.html'
 $uri = [System.Uri]::new($path).AbsoluteUri
 $session = Invoke-HTMLRendering -Url $uri -Session
 Start-Sleep -Milliseconds 500
-Get-HTMLConsoleLog -Session $session | Format-Table
+Get-HTMLConsoleLog -Session $session -Severity Error | Format-Table
 Close-HTMLSession -Session $session
+
 
