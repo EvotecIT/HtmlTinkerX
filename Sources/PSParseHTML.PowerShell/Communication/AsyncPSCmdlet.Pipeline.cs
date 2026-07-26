d06b98887ea484ea877981fc9bd7da6f635df97a
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-07-26 03:16:21 +0200

    Harden async pipeline edge cases

--- Sources/PSParseHTML.PowerShell/Communication/AsyncPSCmdlet.Pipeline.cs
+++ Sources/PSParseHTML.PowerShell/Communication/AsyncPSCmdlet.Pipeline.cs
@@ -107,7 +107,9 @@ public abstract partial class AsyncPSCmdlet
         => _pipelineThreadId != 0 && Environment.CurrentManagedThreadId == _pipelineThreadId;
 
     private bool CanAccessPipelineDirectly
-        => IsPipelineThread || Volatile.Read(ref _asyncLifecycleStarted) == 0;
+        => IsPipelineThread ||
+           (Volatile.Read(ref _asyncLifecycleStarted) == 0 &&
+            Environment.CurrentManagedThreadId == _constructionThreadId);
 
     private void PrepareDirectPipelineAccess()
     {
@@ -168,7 +170,16 @@ public abstract partial class AsyncPSCmdlet
 
         try
         {
-            var reply = replyPipe.Take(CancelToken);
+            PipelineReply reply;
+            try
+            {
+                reply = replyPipe.Take(CancelToken);
+            }
+            catch (OperationCanceledException) when (_cancelSource.IsCancellationRequested)
+            {
+                throw new PipelineStoppedException();
+            }
+
             if (reply.Rejection is not null)
                 throw reply.Rejection;
 
@@ -225,6 +236,7 @@ public abstract partial class AsyncPSCmdlet
         Task blockTask;
         var deferPipeDisposal = 0;
         var pipeDisposed = 0;
+        var pumpingQueuedItems = 0;
         var hookGeneration = Interlocked.Increment(ref _nextHookGeneration);
 
         void ClearPipes()
@@ -395,8 +407,18 @@ public abstract partial class AsyncPSCmdlet
 
         void PumpQueuedItems()
         {
-            while (outPipe.TryTake(out var item))
-                PumpItem(item);
+            if (Interlocked.Exchange(ref pumpingQueuedItems, 1) != 0)
+                return;
+
+            try
+            {
+                while (outPipe.TryTake(out var item))
+                    PumpItem(item);
+            }
+            finally
+            {
+                Volatile.Write(ref pumpingQueuedItems, 0);
+            }
         }
 
         Volatile.Write(ref _asyncLifecycleStarted, 1);
